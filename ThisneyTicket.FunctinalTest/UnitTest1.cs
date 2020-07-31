e0e76b7373365cb25a3a81df4d74abe52d949868
Abner <abnerrv@gmail.com>
2020-07-30 21:17:43 -0300

    Add project files.

---
+++ ThisneyTicket.FunctinalTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ThisneyTicket.FunctinalTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
